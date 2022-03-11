029737f855aac7b4e4923f14d5a26f21c52b34c1
thstar79 <taehoonk79@gmail.com>
2022-03-11 10:26:36 -0800

    so far...

--- config/database.js
+++ config/database.js
@@ -11,6 +11,11 @@ module.exports = {
         password,
         database,
         host,
-        dialect: 'postgres'
+        dialect: 'postgres',
+    },
+    production: {
+        use_env_variable: 'DATABASE_URL',
+        dialect: 'postgres',
+        seederStorage; 'sequelize',
     }
 }
