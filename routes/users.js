d5cf58d36ea7724f97c47e422ea90c28e5cacce0
Steve Choi <stevech071@gmail.com>
2022-03-15 09:46:20 -0700

    Variable names changed and db query where updated so the website functions

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ const { csrfProtection, asyncHandler } = require("./utils");
 
 /* GET users listing. */
 router.get(
-    "/users/:id(\\d+)",
+    "/:id(\\d+)",
     csrfProtection,
     asyncHandler(async(req, res, next) => {
         const user = await db.User.findByPk(req.params.id);
@@ -16,7 +16,6 @@ router.get(
 );
 
 const userValidators = [
-<<<<<<< HEAD
     // TODO Define the user validators.
     check("firstName")
     .exists({ checkFalsy: true })
@@ -41,7 +40,7 @@ const userValidators = [
     .isEmail()
     .withMessage("Email Address is not a valid email")
     .custom((value) => {
-        return db.User.findOne({ where: { emailAddress: value } }).then(
+        return db.User.findOne({ where: { email: value } }).then(
             (user) => {
                 if (user) {
                     return Promise.reject(
@@ -76,7 +75,7 @@ const userValidators = [
 ];
 
 router.get(
-    "/user/register",
+    "/register",
     csrfProtection,
     asyncHandler(async(req, res) => {
         const user = db.User.build();
@@ -89,7 +88,7 @@ router.get(
 );
 
 router.post(
-    "/user/register",
+    "/register",
     csrfProtection,
     userValidators,
     asyncHandler(async(req, res) => {
@@ -126,62 +125,4 @@ router.post(
 );
 
 module.exports = router;
-=======
-  // TODO Define the user validators.
-check('firstName')
-  .exists({ checkFalsy: true })
-  .withMessage('Please provide a value for First Name')
-  .isLength({ max: 50 })
-  .withMessage('First Name must not be more than 50 characters long'),
-check('lastName')
-  .exists({ checkFalsy: true })
-  .withMessage('Please provide a value for Last Name')
-  .isLength({ max: 50 })
-  .withMessage('Last Name must not be more than 50 characters long'),
-check('userName')
-  .exists({ checkFalsy: true })
-  .withMessage('Please provide a value for Username')
-  .isLength({ max: 50 })
-  .withMessage('Last Name must not be more than 50 characters long'),
-check('email')
-  .exists({ checkFalsy: true })
-  .withMessage('Please provide a value for Email Address')
-  .isLength({ max: 255 })
-  .withMessage('Email Address must not be more than 255 characters long')
-  .isEmail()
-  .withMessage('Email Address is not a valid email')
-  .custom((value) => {
-    return db.User.findOne({ where: { emailAddress: value } })
-      .then((user) => {
-        if (user) {
-          return Promise.reject('The provided Email Address is already in use by another account');
-        }
-      });
-  }),  
-check('password')
-  .exists({ checkFalsy: true })
-  .withMessage('Please provide a value for Password')
-  .isLength({ max: 50 })
-  .withMessage('Password must not be more than 50 characters long')
-  .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, 'g')
-  .withMessage('Password must contain at least 1 lowercase letter, uppercase letter, number, and special character (i.e. "!@#$%^&*")'),
-check('confirmPassword')
-  .exists({ checkFalsy: true })
-  .withMessage('Please provide a value for Confirm Password')
-  .isLength({ max: 50 })
-  .withMessage('Confirm Password must not be more than 50 characters long')
-  .custom((value, { req }) => {
-    if (value !== req.body.password) {
-      throw new Error('Confirm Password does not match Password');
-    }
-    return true;
-  }),
 
-];
-
-
-
-module.exports = router;
-
-
->>>>>>> 56cb2dd59260d551d310e0aba3c73906181f85cc
