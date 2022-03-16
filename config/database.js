5ca5524472836408ed4eb7efb932ede4224cba31
Steve Choi <stevech071@gmail.com>
2022-03-16 15:49:38 -0700

    initial upload

--- config/database.js
+++ config/database.js
@@ -4,11 +4,15 @@ let {
 
 module.exports = {
     development: {
-        username,
-        password,
-        database,
-        host,
-        dialect: "postgres",
-        seederStorage: "sequelize",
+      username,
+      password,
+      database,
+      host,
+      dialect: 'postgres',
     },
-};
+    production: {
+      use_env_variable: 'DATABASE_URL',
+      dialect: 'postgres',
+      seederStorage: 'sequelize',
+    }
+  };
