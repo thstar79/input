09c0d9f25cceb0893f8f7efc08584bb8f216b3e8
haywood-d-johnson <haywood.d.johnson@gmail.com>
2022-03-16 15:08:03 -0500

    feat: story delete and redirect

--- db/models/story.js
+++ db/models/story.js
@@ -2,20 +2,26 @@
 
 module.exports = (sequelize, DataTypes) => {
     const Story = sequelize.define(
-        "Story",
-        {
+        "Story", {
             title: DataTypes.STRING,
             content: DataTypes.TEXT,
             topicType: DataTypes.STRING,
             userId: DataTypes.INTEGER,
             gameId: DataTypes.INTEGER,
-        },
-        {}
+        }, {}
     );
 
-    Story.associate = function (models) {
-        Story.hasMany(models.Comment, { foreignKey: "storyId" });
-        Story.hasMany(models.StoryCoin, { foreignKey: "storyId" });
+    Story.associate = function(models) {
+        Story.hasMany(models.Comment, {
+            foreignKey: "storyId",
+            hooks: true,
+            onDelete: "cascade",
+        });
+        Story.hasMany(models.StoryCoin, {
+            foreignKey: "storyId",
+            hooks: true,
+            onDelete: "cascade",
+        });
         Story.belongsTo(models.Game, { foreignKey: "gameId" });
         Story.belongsTo(models.User, { foreignKey: "userId" });
     };
--- routes/stories.js
+++ routes/stories.js
@@ -3,60 +3,60 @@ const { check, validationResult } = require("express-validator");
 const router = express.Router();
 const db = require("../db/models");
 const { csrfProtection, asyncHandler } = require("./utils");
-const { requireAuth } = require("../auth");
 
 router.get(
-  "/stories/:id(\\d+)",
-  csrfProtection,
-  asyncHandler(async (req, res) => {
-    const storyId = parseInt(req.params.id, 10);
-    const story = await db.Story.findByPk(storyId, { include: [db.User, db.Game] });
-    const coins = await db.CommentCoin.findAll({
-      include: [{
-        model: db.Comment,
-        where: {
-          storyId: storyId,
-        },
-      },
-      {
-        model: db.User,
-      }],
-    });
+    "/stories/:id(\\d+)",
+    csrfProtection,
+    asyncHandler(async(req, res) => {
+        const storyId = parseInt(req.params.id, 10);
+        const story = await db.Story.findByPk(storyId, {
+            include: [db.User, db.Game],
+        });
+        const coins = await db.CommentCoin.findAll({
+            include: [{
+                    model: db.Comment,
+                    where: {
+                        storyId: storyId,
+                    },
+                },
+                {
+                    model: db.User,
+                },
+            ],
+        });
 
-    res.render("story-detail", {
-      title: "Detailed Story",
-      story,
-      coins,
-      csrfToken: req.csrfToken()
-    });
-  })
+        res.render("story-detail", {
+            title: "Detailed Story",
+            story,
+            coins,
+            csrfToken: req.csrfToken(),
+        });
+    })
 );
-
 
 const storyValidator = [
     check("title")
-        .exists({ checkFalsy: true })
-        .withMessage("Please provide a value for Title")
-        .isLength({ max: 50 })
-        .withMessage("Title must not be more than 50 characters long"),
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a value for Title")
+    .isLength({ max: 50 })
+    .withMessage("Title must not be more than 50 characters long"),
     check("content")
-        .exists({ checkFalsy: true })
-        .withMessage("Please provide a value for Content"),
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a value for Content"),
     check("topicType")
-        .exists({ checkFalsy: true })
-        .withMessage("Please provide a value for Topic Type")
-        .isLength({ max: 50 })
-        .withMessage("Topic Type must not be more than 50 characters long"),
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a value for Topic Type")
+    .isLength({ max: 50 })
+    .withMessage("Topic Type must not be more than 50 characters long"),
 ];
 
 router.get(
     "/stories/new",
-    requireAuth,
     csrfProtection,
-    asyncHandler(async (req, res) => {
+    asyncHandler(async(req, res) => {
         const story = db.Story.build();
-        const games = await db.Game.findAll()
-        console.log(games)
+        const games = await db.Game.findAll();
+        //console.log(games);
         res.render("story-new", {
             title: "Write a new Story",
             story,
@@ -70,17 +70,20 @@ router.post(
     "/stories/new",
     csrfProtection,
     storyValidator,
-    asyncHandler(async (req, res) => {
-        const { title, content, topicType, gameId, gameTitle } = req.body;        
+    asyncHandler(async(req, res) => {
+        const { title, content, topicType, gameId, gameTitle } = req.body;
         let story = db.Story.build({
             title,
             content,
             topicType,
             gameId,
-            userId: res.locals.user.id, 
+            userId: res.locals.user.id,
         });
 
-        const games = await db.Game.findAll();
+        let game = db.Game.build({
+            id: gameId,
+            title: gameTitle,
+        });
 
         const validatorErrors = validationResult(req);
 
@@ -98,7 +101,7 @@ router.post(
             const errors = validatorErrors.array().map((error) => error.msg);
             res.render("story-new", {
                 title: "Write a Story",
-                games,
+                games: [game],
                 story,
                 errors,
                 csrfToken: req.csrfToken(),
@@ -107,41 +110,76 @@ router.post(
     })
 );
 
-router.get('/stories/edit/:id(\\d+)', csrfProtection, asyncHandler(async(req, res) => {
-    const storyId = parseInt(req.params.id, 10);
-    const story = await db.Story.findByPk(storyId);
-    const games = await db.Game.findAll();
-    res.render('story-edit', {
-        title: 'Edit Story',
-        story,
-        games,
-        csrfToken: req.csrfToken()
+router.get(
+    "/stories/edit/:id(\\d+)",
+    csrfProtection,
+    asyncHandler(async(req, res) => {
+        const storyId = parseInt(req.params.id, 10);
+        const story = await db.Story.findByPk(storyId);
+        const games = await db.Game.findAll();
+        res.render("story-edit", {
+            title: "Edit Story",
+            story,
+            games,
+            csrfToken: req.csrfToken(),
+        });
     })
-}))
+);
 
-router.post('/stories/edit/:id(\\d+)', csrfProtection, storyValidator, asyncHandler(async(req, res) => {
-    const storyId = parseInt(req.params.id, 10);
-    const storyToUpdate = await db.Story.findByPk(storyId);
+router.post(
+    "/stories/edit/:id(\\d+)",
+    csrfProtection,
+    storyValidator,
+    asyncHandler(async(req, res) => {
+        const storyId = parseInt(req.params.id, 10);
+        const storyToUpdate = await db.Story.findByPk(storyId);
 
-    const { title, content, topicType, userId, gameId } = req.body;
-    const story = { title, content, topicType, userId, gameId }
+        const { title, content, topicType, userId, gameId } = req.body;
+        const story = { title, content, topicType, userId, gameId };
 
-    const validatorErrors = validationResult(req);
+        const validatorErrors = validationResult(req);
 
-    if (validatorErrors.isEmpty()) {
-        await storyToUpdate.update(story)
-        res.redirect(`/stories/${storyId}`)
-    } else {
-        const errors = validatorErrors.array().map((error) => error.msg);
-        res.render('story-edit', {
-            title: 'Edit Story',
-            story: { ...story, id: storyId },
-            errors,
-            csrfToken: req.csrfToken()
-        })
-    }
-}))
+        if (validatorErrors.isEmpty()) {
+            await storyToUpdate.update(story);
+            res.redirect(`/stories/${storyId}`);
+        } else {
+            const errors = validatorErrors.array().map((error) => error.msg);
+            res.render("story-edit", {
+                title: "Edit Story",
+                story: {...story, id: storyId },
+                errors,
+                csrfToken: req.csrfToken(),
+            });
+        }
+    })
+);
 
+router.get(
+    "/stories/delete/:id(\\d+)",
+    csrfProtection,
+    asyncHandler(async(req, res) => {
+        const id = parseInt(req.params.id, 10);
 
+        const story = await db.Story.findByPk(id);
+        res.render("story-delete", {
+            title: "Delete this Story???",
+            story,
+            csrfToken: req.csrfToken(),
+        });
+    })
+);
+
+router.post(
+    "/stories/delete/:id(\\d+)",
+    asyncHandler(async(req, res) => {
+        // const id = parseInt(req.params.id, 10);
+
+        // const story = await db.Story.findByPk(id);
+
+        await story.destroy();
+
+        res.redirect("/");
+    })
+);
 
 module.exports = router;
