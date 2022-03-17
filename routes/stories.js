02ffdf6778592925d83ef4df17bb5f91f10b71c7
Steve Choi <stevech071@gmail.com>
2022-03-17 16:11:07 -0700

    User registration and login redirect routes updated for new splash page route

--- routes/stories.js
+++ routes/stories.js
@@ -51,12 +51,15 @@ router.get(
                 storyId: storyId,
             }
         });
-        const userStoryCoinsCount = await db.StoryCoin.sum('count', {
-            where: { userId: res.locals.user.id}
-        })
-        if (!userStoryCoinsCount) {
-            let userStoryCoinsCount = 0
-        } 
+        let userStoryCoinsCount;
+        if (req.session.auth) {
+            const { userId } = req.session.auth;
+            userStoryCoinsCount = await db.StoryCoin.sum('count', {
+                where: { userId: userId}
+            })
+        } else {
+            userStoryCoinsCount = 0
+        }
 
         res.render("story-detail", {
             title: "Detailed Story",
