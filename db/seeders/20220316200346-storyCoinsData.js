10a16c8d8e424f1ebb7a52a954f770b5c0fe0870
Jared Kunhart <hakujaden@comcast.net>
2022-03-19 20:40:46 -0500

    final seed for heroku

--- db/seeders/20220316200346-storyCoinsData.js
+++ db/seeders/20220316200346-storyCoinsData.js
@@ -4,10 +4,7 @@ module.exports = {
   async up (queryInterface, Sequelize) {
 
     await queryInterface.bulkInsert('StoryCoins', [
-      { count: 30, storyId: 1, userId: 1, createdAt: new Date(), updatedAt: new Date()},
-      { count: 25, storyId: 2, userId: 1, createdAt: new Date(), updatedAt: new Date()},
-      { count: 32, storyId: 1, userId: 2, createdAt: new Date(), updatedAt: new Date()},
-      { count: 50, storyId: 1, userId: 3, createdAt: new Date(), updatedAt: new Date()},
+      { count: 10, storyId: 1, userId: 1, createdAt: new Date(), updatedAt: new Date()}
     ], {});
 
   },
