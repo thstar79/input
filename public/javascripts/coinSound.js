3c4e240a9da4546eb2292f651bbab4501aa21afa
thstar79 <taehoonk79@gmail.com>
2022-03-21 07:08:39 -0700

    sound animation

--- public/javascripts/coinSound.js
+++ public/javascripts/coinSound.js
@@ -1,19 +1,20 @@
-window.addEventListener("DOMContentLoaded", event => {
-    let audio = new Audio("/assets/smb_coin.wav");
+// window.addEventListener("DOMContentLoaded", event => {
+//     let audio = new Audio("/assets/smb_coin.wav");
 
-    const coins = document.getElementsByClassName('coinSound');
-    for(let i=0;i<coins.length;++i){
-        coins[i].addEventListener('click',  () =>  {
-            audio.volume = 0.1;
-            audio.play();
-        })
-    }
+//     const coins = document.getElementsByClassName('coinSound');
+//     for(let i=0;i<coins.length;++i){
+//         coins[i].addEventListener('click',  () =>  {
+//             console.log("sound clicked");
+//             audio.volume = 0.1;
+//             audio.play();
+//         })
+//     }
 
-    // for(let i=0;i<coins.length;++i){
-    //     coins[i].addEventListener('click',  () =>  {
-    //         audio.volume = 0.1;
-    //         audio.play();
-    //     })
-    // }
-})
+//     // for(let i=0;i<coins.length;++i){
+//     //     coins[i].addEventListener('click',  () =>  {
+//     //         audio.volume = 0.1;
+//     //         audio.play();
+//     //     })
+//     // }
+// })
 
