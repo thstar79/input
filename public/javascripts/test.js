79481684ed746f118af862b8c7902d04bbcf0e28
thstar79 <taehoonk79@gmail.com>
2022-03-20 20:22:04 -0700

    delete sound for login page

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -1,10 +1,10 @@
 window.addEventListener("DOMContentLoaded", event => {
-    let audio = new Audio("../assets/smb_stage_clear.wav");
+    // let audio = new Audio("../assets/smb_stage_clear.wav");
 
-    document.getElementById('testButton').addEventListener('click', () =>  {
-        audio.volume = 0.1;
-        audio.play();
-    })
+    // document.getElementById('testButton').addEventListener('click', () =>  {
+    //     audio.volume = 0.1;
+    //     audio.play();
+    // })
     document.getElementById('registerLink').addEventListener('click', (e) => {
         e.preventDefault();
         
